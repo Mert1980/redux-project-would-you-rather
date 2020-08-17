2eaa8579598d9eab3c8718f8e5f3f90120c43932
Mert <mertdemirok80@gmail.com>
2020-08-17 12:46:26 +0200

    initial commit, setup api

--- _DATA.js
+++ _DATA.js
@@ -1,137 +1,140 @@
 let users = {
-  sarahedo: {
-    id: 'sarahedo',
-    name: 'Sarah Edo',
-    avatarURL: ,
+  ilhan: {
+    id: "ilhan",
+    name: "Ilhan",
+    avatarURL: "https://ca.slack-edge.com/T91PPTG9H-UNJB00M9T-0134bbcb5a0b-512",
     answers: {
-      "8xf0y6ziyjabvozdd253nd": 'optionOne',
-      "6ni6ok3ym7mf1p33lnez": 'optionTwo',
-      "am8ehyc8byjqgar0jgpub9": 'optionTwo',
-      "loxhs1bqm25b708cmbf3g": 'optionTwo'
+      "8xf0y6ziyjabvozdd253nd": "optionOne",
+      "6ni6ok3ym7mf1p33lnez": "optionTwo",
+      am8ehyc8byjqgar0jgpub9: "optionTwo",
+      loxhs1bqm25b708cmbf3g: "optionTwo",
     },
-    questions: ['8xf0y6ziyjabvozdd253nd', 'am8ehyc8byjqgar0jgpub9']
+    questions: ["8xf0y6ziyjabvozdd253nd", "am8ehyc8byjqgar0jgpub9"],
   },
-  tylermcginnis: {
-    id: 'tylermcginnis',
-    name: 'Tyler McGinnis',
-    avatarURL: ,
+  mert: {
+    id: "mert",
+    name: "Mert Demirok",
+    avatarURL: "https://ca.slack-edge.com/T91PPTG9H-UMYFQRD8T-c3c7c480793b-512",
     answers: {
-      "vthrdm985a262al8qx3do": 'optionOne',
-      "xj352vofupe1dqz9emx13r": 'optionTwo',
+      vthrdm985a262al8qx3do: "optionOne",
+      xj352vofupe1dqz9emx13r: "optionTwo",
     },
-    questions: ['loxhs1bqm25b708cmbf3g', 'vthrdm985a262al8qx3do'],
+    questions: ["loxhs1bqm25b708cmbf3g", "vthrdm985a262al8qx3do"],
   },
-  johndoe: {
-    id: 'johndoe',
-    name: 'John Doe',
-    avatarURL: ,
+  ibrahim: {
+    id: "ibrahim",
+    name: "Ibrahim Dogrusoz",
+    avatarURL: "https://ca.slack-edge.com/T91PPTG9H-UF6CPFY2F-2a06a951b1e8-512",
     answers: {
-      "xj352vofupe1dqz9emx13r": 'optionOne',
-      "vthrdm985a262al8qx3do": 'optionTwo',
-      "6ni6ok3ym7mf1p33lnez": 'optionTwo'
+      xj352vofupe1dqz9emx13r: "optionOne",
+      vthrdm985a262al8qx3do: "optionTwo",
+      "6ni6ok3ym7mf1p33lnez": "optionTwo",
     },
-    questions: ['6ni6ok3ym7mf1p33lnez', 'xj352vofupe1dqz9emx13r'],
-  }
-}
+    questions: ["6ni6ok3ym7mf1p33lnez", "xj352vofupe1dqz9emx13r"],
+  },
+};
 
 let questions = {
   "8xf0y6ziyjabvozdd253nd": {
-    id: '8xf0y6ziyjabvozdd253nd',
-    author: 'sarahedo',
+    id: "8xf0y6ziyjabvozdd253nd",
+    author: "ilhan",
     timestamp: 1467166872634,
     optionOne: {
-      votes: ['sarahedo'],
-      text: 'have horrible short term memory',
+      votes: [],
+      text: "have horrible short term memory",
     },
     optionTwo: {
       votes: [],
-      text: 'have horrible long term memory'
-    }
+      text: "have horrible long term memory",
+    },
   },
   "6ni6ok3ym7mf1p33lnez": {
-    id: '6ni6ok3ym7mf1p33lnez',
-    author: 'johndoe',
+    id: "6ni6ok3ym7mf1p33lnez",
+    author: "ibrahim",
     timestamp: 1468479767190,
     optionOne: {
       votes: [],
-      text: 'become a superhero',
+      text: "become a superhero",
     },
     optionTwo: {
-      votes: ['johndoe', 'sarahedo'],
-      text: 'become a supervillain'
-    }
+      votes: ["ibrahim", "ilhan"],
+      text: "become a supervillain",
+    },
   },
-  "am8ehyc8byjqgar0jgpub9": {
-    id: 'am8ehyc8byjqgar0jgpub9',
-    author: 'sarahedo',
+  am8ehyc8byjqgar0jgpub9: {
+    id: "am8ehyc8byjqgar0jgpub9",
+    author: "ilhan",
     timestamp: 1488579767190,
     optionOne: {
       votes: [],
-      text: 'be telekinetic',
+      text: "be telekinetic",
     },
     optionTwo: {
-      votes: ['sarahedo'],
-      text: 'be telepathic'
-    }
+      votes: ["ilhan"],
+      text: "be telepathic",
+    },
   },
-  "loxhs1bqm25b708cmbf3g": {
-    id: 'loxhs1bqm25b708cmbf3g',
-    author: 'tylermcginnis',
+  loxhs1bqm25b708cmbf3g: {
+    id: "loxhs1bqm25b708cmbf3g",
+    author: "mert",
     timestamp: 1482579767190,
     optionOne: {
       votes: [],
-      text: 'be a front-end developer',
+      text: "be a front-end developer",
     },
     optionTwo: {
-      votes: ['sarahedo'],
-      text: 'be a back-end developer'
-    }
+      votes: ["ilhan"],
+      text: "be a back-end developer",
+    },
   },
-  "vthrdm985a262al8qx3do": {
-    id: 'vthrdm985a262al8qx3do',
-    author: 'tylermcginnis',
+  vthrdm985a262al8qx3do: {
+    id: "vthrdm985a262al8qx3do",
+    author: "mert",
     timestamp: 1489579767190,
     optionOne: {
-      votes: ['tylermcginnis'],
-      text: 'find $50 yourself',
+      votes: ["mert"],
+      text: "find $50 yourself",
     },
     optionTwo: {
-      votes: ['johndoe'],
-      text: 'have your best friend find $500'
-    }
+      votes: ["ibrahim"],
+      text: "have your best friend find $500",
+    },
   },
-  "xj352vofupe1dqz9emx13r": {
-    id: 'xj352vofupe1dqz9emx13r',
-    author: 'johndoe',
+  xj352vofupe1dqz9emx13r: {
+    id: "xj352vofupe1dqz9emx13r",
+    author: "ibrahim",
     timestamp: 1493579767190,
     optionOne: {
-      votes: ['johndoe'],
-      text: 'write JavaScript',
+      votes: ["ibrahim"],
+      text: "write JavaScript",
     },
     optionTwo: {
-      votes: ['tylermcginnis'],
-      text: 'write Swift'
-    }
+      votes: ["mert"],
+      text: "write Swift",
+    },
   },
+};
+
+function generateUID() {
+  return (
+    Math.random().toString(36).substring(2, 15) +
+    Math.random().toString(36).substring(2, 15)
+  );
 }
 
-function generateUID () {
-  return Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15)
-}
-
-export function _getUsers () {
+export function _getUsers() {
   return new Promise((res, rej) => {
-    setTimeout(() => res({...users}), 1000)
-  })
+    setTimeout(() => res({ ...users }), 1000);
+  });
 }
 
-export function _getQuestions () {
+export function _getQuestions() {
   return new Promise((res, rej) => {
-    setTimeout(() => res({...questions}), 1000)
-  })
+    setTimeout(() => res({ ...questions }), 1000);
+  });
 }
 
-function formatQuestion ({ optionOneText, optionTwoText, author }) {
+function formatQuestion({ optionOneText, optionTwoText, author }) {
   return {
     id: generateUID(),
     timestamp: Date.now(),
@@ -143,11 +146,11 @@ function formatQuestion ({ optionOneText, optionTwoText, author }) {
     optionTwo: {
       votes: [],
       text: optionTwoText,
-    }
-  }
+    },
+  };
 }
 
-export function _saveQuestion (question) {
+export function _saveQuestion(question) {
   return new Promise((res, rej) => {
     const authedUser = question.author;
     const formattedQuestion = formatQuestion(question);
@@ -155,23 +158,23 @@ export function _saveQuestion (question) {
     setTimeout(() => {
       questions = {
         ...questions,
-        [formattedQuestion.id]: formattedQuestion
-      }
-      
+        [formattedQuestion.id]: formattedQuestion,
+      };
+
       users = {
         ...users,
         [authedUser]: {
           ...users[authedUser],
-          questions: users[authedUser].questions.concat([formattedQuestion.id])
-        }
-      }
+          questions: users[authedUser].questions.concat([formattedQuestion.id]),
+        },
+      };
 
-      res(formattedQuestion)
-    }, 1000)
-  })
+      res(formattedQuestion);
+    }, 1000);
+  });
 }
 
-export function _saveQuestionAnswer ({ authedUser, qid, answer }) {
+export function _saveQuestionAnswer({ authedUser, qid, answer }) {
   return new Promise((res, rej) => {
     setTimeout(() => {
       users = {
@@ -180,10 +183,10 @@ export function _saveQuestionAnswer ({ authedUser, qid, answer }) {
           ...users[authedUser],
           answers: {
             ...users[authedUser].answers,
-            [qid]: answer
-          }
-        }
-      }
+            [qid]: answer,
+          },
+        },
+      };
 
       questions = {
         ...questions,
@@ -191,12 +194,12 @@ export function _saveQuestionAnswer ({ authedUser, qid, answer }) {
           ...questions[qid],
           [answer]: {
             ...questions[qid][answer],
-            votes: questions[qid][answer].votes.concat([authedUser])
-          }
-        }
-      }
+            votes: questions[qid][answer].votes.concat([authedUser]),
+          },
+        },
+      };
 
-      res()
-    }, 500)
-  })
+      res();
+    }, 500);
+  });
 }
