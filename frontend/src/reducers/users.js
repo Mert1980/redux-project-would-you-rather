c785f44be6e84d06ead11e9c4f989a5c637664c3
Mert <mertdemirok80@gmail.com>
2020-09-01 00:06:02 +0200

    handle dispatch addQuestion in users reducer

--- frontend/src/reducers/users.js
+++ frontend/src/reducers/users.js
@@ -1,5 +1,6 @@
 import { RECEIVE_USERS } from "../actions/users";
 import { SAVE_ANSWER } from "../actions/saveAnswer";
+import { ADD_QUESTION } from "../actions/addQuestion";
 
 export default function users(state = {}, action) {
   switch (action.type) {
@@ -22,6 +23,15 @@ export default function users(state = {}, action) {
             ...state[authedUser].answers,
             ...newAnswer,
           },
+        },
+      };
+    case ADD_QUESTION:
+      const { author, id } = action.question;
+      return {
+        ...state,
+        [author]: {
+          ...state[author],
+          questions: [id, ...state[author].questions],
         },
       };
     default:
